1098be55d24fbc56a559766d7c3d679ddf9034cf
S4vi0r <gbp17@outlook.com>
2024-10-12 00:08:57 -0500

    Refactor ScreenmatchApplication to utilize Principal class for user interaction; implement series search functionality

--- 01-screenmatch/src/main/java/com/savior/screenmatch/ScreenmatchApplication.java
+++ 01-screenmatch/src/main/java/com/savior/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package com.savior.screenmatch;
 
-import com.savior.screenmatch.model.DatosEpisodio;
-import com.savior.screenmatch.model.DatosSerie;
-import com.savior.screenmatch.model.DatosTemporadas;
-import com.savior.screenmatch.service.ConsumoAPI;
-import com.savior.screenmatch.service.ConvierteDatos;
+import com.savior.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,26 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=991b34ae");
-		// var json = consumoAPI.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConvierteDatos convierteDatos = new ConvierteDatos();
-		var datosSerie = convierteDatos.obtenerDatos(json, DatosSerie.class);
-		System.out.println(datosSerie);
-
-		json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&season=1&episode=1&apikey=991b34ae");
-		System.out.println(json);
-		DatosEpisodio datosEpisodio = convierteDatos.obtenerDatos(json, DatosEpisodio.class);
-		System.out.println(datosEpisodio);
-
-		List<DatosTemporadas> temporadas = new ArrayList<>();
-		for (int i = 1; i <= 8; i++) {
-			json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&season=" + i + "&apikey=991b34ae");
-			// System.out.println(json);
-			var datosTemporadas = convierteDatos.obtenerDatos(json, DatosTemporadas.class);
-			temporadas.add(datosTemporadas);
-		}
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.muestraMenu();
 	}
 }
--- 01-screenmatch/src/main/java/com/savior/screenmatch/principal/Principal.java
+++ 01-screenmatch/src/main/java/com/savior/screenmatch/principal/Principal.java
@@ -1,2 +1,35 @@
-package com.savior.screenmatch.principal;public class Principal {
+package com.savior.screenmatch.principal;
+
+import com.savior.screenmatch.model.DatosSerie;
+import com.savior.screenmatch.model.DatosTemporadas;
+import com.savior.screenmatch.service.ConsumoAPI;
+import com.savior.screenmatch.service.ConvierteDatos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner scanner = new Scanner(System.in);
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private final String API = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=991b34ae";
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    public void muestraMenu() {
+        System.out.print("Escribe el nombre de la serie que deseas buscar: ");
+        var serie = scanner.nextLine();
+        var json = consumoAPI.obtenerDatos(API + serie.replace(" ", "+") + API_KEY);
+        var datosSerie = conversor.obtenerDatos(json, DatosSerie.class);
+        System.out.println(datosSerie);
+
+        List<DatosTemporadas> temporadas = new ArrayList<>();
+        for (int i = 1; i <= datosSerie.totalTemporadas(); i++) {
+            json = consumoAPI.obtenerDatos(API + serie.replace(" ", "+") + "&season=" + i + API_KEY);
+            // System.out.println(json);
+            var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
+            temporadas.add(datosTemporadas);
+        }
+        temporadas.forEach(System.out::println);
+    }
 }
