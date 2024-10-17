37ccf11a5582cac51fe24541837bcac6eb9f552a
S4vi0r <gbp17@outlook.com>
2024-10-17 15:26:29 -0500

    Fix typo in DatosLibro field name and add functionality to display top 10 downloaded books in Principal class

--- 02-gutendex/src/main/java/com/example/savior/gutendex/model/DatosLibro.java
+++ 02-gutendex/src/main/java/com/example/savior/gutendex/model/DatosLibro.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosLibro(
-        @JsonAlias("tittle")
+        @JsonAlias("title")
         String titulo,
         @JsonAlias("authors")
         List<DatosAutor> autores,
--- 02-gutendex/src/main/java/com/example/savior/gutendex/principal/Principal.java
+++ 02-gutendex/src/main/java/com/example/savior/gutendex/principal/Principal.java
@@ -1,8 +1,11 @@
 package com.example.savior.gutendex.principal;
 
 import com.example.savior.gutendex.model.Datos;
+import com.example.savior.gutendex.model.DatosLibro;
 import com.example.savior.gutendex.service.ConsumoAPI;
 import com.example.savior.gutendex.service.ConvierteDatos;
+
+import java.util.Comparator;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
@@ -14,5 +17,13 @@ public class Principal {
         System.out.println(json);
         var datos = convierteDatos.obtenerDatos(json, Datos.class);
         System.out.println(datos);
+
+        // Top 10 de libros más descargados
+        System.out.println("Top 10 de libros más descargados");
+        datos.libros().stream()
+                .sorted(Comparator.comparing(DatosLibro::numeroDescargas).reversed())
+                .limit(10)
+                .map(libro -> libro.titulo().toUpperCase())
+                .forEach(System.out::println);
     }
 }
