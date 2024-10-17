7df790c6bfb98a2ff87c542f20e64efe73860e09
S4vi0r <gbp17@outlook.com>
2024-10-17 14:18:18 -0500

    Initialize Gutendex project with Spring Boot setup; add application properties, Maven wrapper, and basic test class

---
+++ 02-gutendex/src/test/java/com/example/savior/gutendex/GutendexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.savior.gutendex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GutendexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
