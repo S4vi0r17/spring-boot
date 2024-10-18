ab18fd883d7d3cb9bf405a26167e16c2ca06f150
S4vi0r <gbp17@outlook.com>
2024-10-17 19:04:01 -0500

    Add book search functionality to Principal class; allow users to find books by name

--- 02-gutendex/src/main/java/com/example/savior/gutendex/principal/Principal.java
+++ 02-gutendex/src/main/java/com/example/savior/gutendex/principal/Principal.java
@@ -6,11 +6,14 @@ import com.example.savior.gutendex.service.ConsumoAPI;
 import com.example.savior.gutendex.service.ConvierteDatos;
 
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private ConvierteDatos convierteDatos = new ConvierteDatos();
+    private Scanner scanner = new Scanner(System.in);
 
     public void mostrarMenu() {
         var json = consumoAPI.obtenerDatos(URL_BASE);
@@ -25,5 +28,21 @@ public class Principal {
                 .limit(10)
                 .map(libro -> libro.titulo().toUpperCase())
                 .forEach(System.out::println);
+
+        // Busque da de libros por nombre
+        System.out.print("Ingrese el nombre del libro a buscar: ");
+        var nombreLibro = scanner.nextLine();
+        System.out.println("Libros encontrados con el nombre " + nombreLibro);
+        json = consumoAPI.obtenerDatos(URL_BASE + "?search=" + nombreLibro.replace(" ", "+")); // %20
+        datos = convierteDatos.obtenerDatos(json, Datos.class);
+        Optional<DatosLibro> libro = datos.libros().stream()
+                .filter(libro1 -> libro1.titulo().toUpperCase().contains(nombreLibro.toUpperCase()))
+                .findFirst();
+
+        if (libro.isPresent()) {
+            System.out.println("Libro encontrado: " + libro.get());
+        } else {
+            System.out.println("No se encontraron libros con el nombre " + nombreLibro);
+        }
     }
 }
